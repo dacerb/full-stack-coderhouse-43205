07ecc03c6bbfd674826cfeef2ea1c212382fb712
David <david.acerbo@gmail.com>
2023-05-01 14:51:58 -0300

    update

---
+++ clases/43205-clase--1--react/calculadora.js
@@ -0,0 +1,5 @@
+import { suma, resta, division, multi } from "./operaciones.js";
+
+console.log(suma(50,80));
+console.log(resta(20,10));
+
---
+++ clases/43205-clase--1--react/operaciones.js
@@ -0,0 +1,7 @@
+export const suma = (a,b)  => a + b;
+export const resta = (a,b)  => a - b;
+export const division = (a,b)  => a / b;
+export const multi = (a,b)  => a * b;
+
+//export {suma, resta, division, multi};
+
