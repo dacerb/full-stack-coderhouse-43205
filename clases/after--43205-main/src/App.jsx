11bbef5c79d48bbe34027c04dbd941c0937cb727
dacerbo <david.acerbo@mercadolibre.com>
2023-05-13 11:51:31 -0300

    update clases

---
+++ clases/after--43205-main/src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/Navbar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
+function App() {
+  
+
+  return (
+    <>
+     <NavBar />
+     <ItemListContainer greeting={"Hola Mundo"} />
+     <h2>Un Cambio</h2>
+    </>
+  )
+}
+
+export default App
