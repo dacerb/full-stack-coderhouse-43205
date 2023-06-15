8ca8e7658f7d23955d668fd22968af35a73ff4fb
David <david.acerbo@gmail.com>
2023-06-15 02:38:31 -0300

    ajustes en itemDetail para mostrar en celular

--- app/store/src/components/ItemDetail/ItemDetail.jsx
+++ app/store/src/components/ItemDetail/ItemDetail.jsx
@@ -25,15 +25,16 @@ const ItemDetail = ({id, name, price, img, stock, description}) => {
       
       <div className='container  d-flex  justify-content-center flex-wrap gap-4 mt-4 pt-4 itemContainer pt-5 pb-5'>
         
-          <div className='row position-relative'>
+          <div className='row position-relative ' style={{ padding: "10px"}}>
           <Link 
               to={"/"}
-              className='btn-close position-absolute top-0 start-100 translate-middle '
+              className='btn-close position-absolute top-0  translate-middle'
+              style={{ right: "0px"}}
               type="button" 
               aria-label="Close"> 
               </Link>
          
-            <div className="col-12" style={{ width: "30rem" }}>
+            <div className="col-12" style={{ maxWidth: "30rem" }}>
             <img src={img} className="card-img-top" alt={name}/>
             <div className="card-body">
               <h5 className="card-title">{name}</h5>
